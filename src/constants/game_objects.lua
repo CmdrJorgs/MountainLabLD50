d10cff94e0c78f249737f7789d524b608ae90533
crabNebula22 <skyrimsten45@gmail.com>
2022-04-03 18:39:00 -0600

    added tree objects

--- src/constants/game_objects.lua
+++ src/constants/game_objects.lua
@@ -6,5 +6,21 @@ GAME_OBJECT_DEFS = {
         width = 32,
         height = 32,
         solid = true, -- set to true to enable collision detection --
-    }
+    },
+    ['tall_tree'] = {
+        type = 'tree',
+        texture = 'tall_tree',
+        frame = 1,
+        width = 27,
+        height = 31,
+        solid = true, -- set to true to enable collision detection --
+    },
+    ['umbrella_tree'] = {
+        type = 'tree',
+        texture = 'umbrella_tree',
+        frame = 1,
+        width = 32,
+        height = 32,
+        solid = true, -- set to true to enable collision detection --
+    },
 }
