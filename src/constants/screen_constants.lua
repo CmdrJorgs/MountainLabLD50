5b924b656dbacd71ff85eff28ac31326e8940899
Hans Jorgensen <hans@thehans255.com>
2022-04-03 17:11:37 -0700

    Move game configuration to conf.lua
    - This should cause the console to have useful data on Windows

---
+++ src/constants/screen_constants.lua
@@ -0,0 +1,8 @@
+-- Constants for the screen. Kept in a separate file
+-- so that conf.lua can access them too
+
+VIRTUAL_WIDTH = 768
+VIRTUAL_HEIGHT = 432
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
