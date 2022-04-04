97d71ff24320d114a3e10acdba8d76dd5d519fb8
crabNebula22 <skyrimsten45@gmail.com>
2022-04-03 21:34:41 -0600

    text for volcano

--- src/constants/text.lua
+++ src/constants/text.lua
@@ -1,6 +1,6 @@
 gText = {
-    ['gameOverText'] = 'You heccin\' died!',
-    ['introText'] = 'You are the Pontifex in Rome and Vesuvius is rumbling. Help the villagers avoid certain destruction by divining what should be sacrificed! Drag objects in the volcano to see if it can be pacified',
+    ['gameOverText'] = 'Interfectus Es ...You heccin\' died!',
+    ['introText'] = 'You are the Pontifex in Rome and Vesuvius is rumbling. Help the villagers avoid certain destruction by divining what should be sacrificed! Drag objects into the volcano to see if it can be pacified',
     ['startScreen'] = 'press enter to begin',
 }
 
@@ -8,22 +8,38 @@ gVolcanoFeedbackText = {
     ['satisfied'] = {
         ['bird'] = {
             'Actually tastes like chicken!',
+            'That was finger lickin good!',
+            'The chicken must have been hungry...thats a history joke',
+            'Good! I hope it was Ovid\'s',
+        },
+        ['blueTogaHuman'] = {
+            'It\'s Greek to me',
+            'Athena is mad, but I\'m happy',
+        },
+        ['whiteTogaHuman'] = {
+            'Varus give me MORE legions!',
+            'even better than a Caesar salad!',
+        },
+        ['goat'] = {
+            'an excellent gift for Apollo',
         },
         ['sheep'] = {
             'Good mutton!',
-            'sheep specific sentence 2',
-            'sheep specific sentence 3',
-            'sheep specific sentence 4',
+            'Bene Bidens!',
+            'I needed a wool sweater',
         },
         ['default'] = {
-            "That was finger lickin' good!",
             'Tastes like chicken!',
             'Mmm, that hit the spot.',
+            'Vulcan is pleased',
+            'te ago gratis!',
         }
     },
     ['non-matching'] = {
         ['whiteTogaHuman'] = {
-            'Admit it, you just wanted this guy\'s money.',
+            'Admit it, you just wanted this guy\'s sesterces.',
+            'ugh...another bland Marcus',
+            'Quid Struis? close but try a different one',
         },
         ['sheep'] = {
             'Mutton is SOOOOOOOOO A.D. 78.',
